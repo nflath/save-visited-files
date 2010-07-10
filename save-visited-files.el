cff5f8b2e8a3381a1cd43776209ebdb05abd6e8e
Nathaniel Flath <flat0103@gmail.com>
2010-07-10 18:10:21 -0400

    Fixed headers for ELPA

--- save-visited-files.el
+++ save-visited-files.el
@@ -1,14 +1,20 @@
-;; Save-visited-files.el - save opened files across sessions
+;;; Save-visited-files.el - save opened files across sessions
 
 ;; Copyright (C) 2009 Nathaniel Flath <nflath@gmail.com>
+
+;; Author: Nathaniel Flath <nflath@gmail.com>
+;; URL: http://github.com/nflath/save-visited-files
 ;; Version: 1.2
-;;
-;; Commentary
-;; Save-visited-files is a lightweight version of Desktop.el that
+
+;;; Commentary:
+
+;; save-visited-files is a lightweight version of Desktop.el that
 ;; only save the files you have open(currently).  This was created because I
 ;; couldn't ever get Desktop to work and wanted to persist open files across
 ;; sessions.  This file is the result.
-;;
+
+;;; Installation:
+
 ;; To install, put this file somewhere in your load-path and add the following
 ;; to your .emacs file:
 ;;
@@ -21,8 +27,7 @@
 ;; save the visited files at any time, you can call M-x save-visited-files-save.
 ;; M-x save-visited-files-restore will open all files saved this way.  To turn
 ;; off the saving of files, you need to run (turn-off-save-visited-files-mode)
-;;
-;;
+
 ;; Changelog:
 ;; 1.2
 ;;  * Changed default value of save-visited-files-location to ~/.emacs.d/emacs-visisted-files
@@ -40,9 +45,25 @@
 ;;  ** Better handling of the temp buffer
 ;; 1.0
 ;;  * Initial Release
-;;
-;; Code:
 
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
 
 (defcustom save-visited-files-location "~/.emacs.d/emacs-visited-files"
   "Location of the file that contains the list of previously visited files"
@@ -57,6 +78,7 @@
 (defvar save-visited-files-already-restored nil
   "If t, then files have already been restored")
 
+;;;###autoload
 (defun save-visited-files-save (&optional location)
   "Save the list of currently visited files"
   (interactive)
@@ -76,6 +98,7 @@
             (write-file location nil)))
         (kill-buffer (get-buffer "*Save Visited*"))))))
 
+;;;###autoload
 (defun save-visited-files-restore (&optional location)
   "Restore all files that were saved by save-visited-files-save."
   (interactive)
@@ -96,6 +119,7 @@
     (kill-buffer (current-buffer))
     (setq save-visited-files-already-restored t)))
 
+;;;###autoload
 (define-minor-mode save-visited-files-mode
   "Minor mode to automatically save a list of all open files, and
 optionally open all files from such a list at startup."
@@ -118,14 +142,17 @@ optionally open all files from such a list at startup."
       (remove-hook 'kill-emacs-hook 'save-visited-files-save)
       (message "Save visited files mode disabled"))))
 
+;;;###autoload
 (defun turn-on-save-visited-files-mode ()
   "Turns save-visited-files-mode on"
   (interactive)
   (save-visited-files-mode 1))
 
+;;;###autoload
 (defun turn-off-save-visited-files-mode ()
   "Turns save-visited-files-mode off"
   (interactive)
   (setq save-visited-files-mode nil))
 
 (provide 'save-visited-files)
+;;; save-visited-files.el ends here

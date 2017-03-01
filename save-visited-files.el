33e8d223f622001f5792c52d8b36661e46b5834c
Nathaniel Flath <flat0103@gmail.com>
2017-02-28 22:50:05 -0800

    Add to after-init-hook if enabled during initialization. Fix for

--- save-visited-files.el
+++ save-visited-files.el
@@ -4,7 +4,7 @@
 
 ;; Author: Nathaniel Flath <nflath@gmail.com>
 ;; URL: http://github.com/nflath/save-visited-files
-;; Version: 1.3
+;; Version: 1.4
 
 ;;; Commentary:
 
@@ -29,6 +29,9 @@
 ;; off the saving of files, you need to run (turn-off-save-visited-files-mode)
 
 ;; Changelog:
+;; 1.4
+;;  * Add to after-init-hook if run during initialization instead of restoring
+;;  * immediately.
 ;; 1.3
 ;;  * Allow saving of dired directories.
 ;;  * Add save-visited-files-ignore-directories configuration variable.
@@ -167,7 +170,9 @@ optionally open all files from such a list at startup."
         (add-hook 'kill-emacs-hook 'save-visited-files-save)
         (unless save-visited-files-already-restored
           (when save-visited-files-auto-restore
-            (save-visited-files-restore)))
+            (if after-init-time
+                (save-visited-files-restore)
+              (add-hook 'after-init-hook 'save-visited-files-restore))))
         (message "Save visited files mode enabled"))
     ;; deactivate
     (progn
